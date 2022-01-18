21fdcaace9acae679e09b64952410bdcfe7a7f4f
Osama-Mustafa-33 <osama.mustafa.3333@gmail.com>
2022-01-18 11:01:03 +0200

    fix migration, factory, seeder, model

--- database/seeders/AuthorSeeder.php
+++ database/seeders/AuthorSeeder.php
@@ -2,9 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Author;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class AuthorSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -13,7 +14,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call('UsersTableSeeder');
-        $this->call(AuthorSeeder::class);
+        Author::factory()->count(50)->create();
     }
 }
